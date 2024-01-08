d2a2fe7fa8cced0f00305722cd8a284faf27cc5f
Avinashkumar2811 <ak8743653@gmail.com>
2024-01-08 15:45:46 +0530

    main input function

--- Typing Master in Terminal.py
+++ Typing Master in Terminal.py
@@ -74,3 +74,72 @@ def get_user_input():
         
 def display_category_data(category):
     print(f"{PURPLE}{words_dict[category]['paragraph']}{END_COLOR}")
+
+def main():
+    try:
+        print("<=== 🥷 Welcome to Typing Test 🥷 ===>")
+        username, choice = get_user_input()
+        print(f"Selected choice: {choice}")
+        
+        if choice == '1':
+            # Choose a category
+            print(f"{CYAN}Your favorite Category: {END_COLOR} ")
+            print(", ".join(words_dict.keys()))
+
+            selected_category = input(f"{CYAN}\nEnter the Category: {END_COLOR} ")
+
+            # Check if the selected category exists
+            if selected_category in words_dict:
+                # Display category data
+                display_category_data(selected_category)
+
+                # Start typing test with words from the selected category
+                words = words_dict[selected_category]['paragraph'].split()
+
+                print(f"\nType the following paragraph related to '{selected_category}':")
+
+                start_time = time.time()
+
+                user_input = input("Start typing: ")
+
+                end_time = time.time()
+
+                time_taken = end_time - start_time
+                words_typed = len(user_input.split())
+                wpm = int((words_typed / time_taken) * 60) if time_taken > 0 else 0
+
+                # Check for errors in the typed text
+                errors = sum(c1 != c2 for c1, c2 in zip(user_input, words_dict[selected_category]['paragraph']))
+
+                print(f"{CYAN}\n{username.capitalize()} Your Stats: {END_COLOR}")
+                print(f"Words Typed: {words_typed}\nTime Taken: {time_taken:.2f} seconds\nWords Per Minute: {wpm} WPM")
+                print(f"Errors: {errors}")
+
+                update_leaderboard(username, words_typed, time_taken, wpm)
+
+                # Ask if the user wants to exit or continue
+                exit_choice = input("\nDo you want to exit? (y/n): ").lower()
+                if exit_choice == 'y':
+                    print("Goodbye!")
+                    print("Exiting...")
+                    exit()
+            else:
+                print(f"{RED}Invalid category: {selected_category}. Please choose a valid category.{END_COLOR}")
+
+        elif choice == '2':
+            show_leaderboard()
+
+        elif choice == '3':
+            print("Goodbye!")
+            print("Exiting...")
+            exit()
+        else:
+            print("Invalid choice. Please try again.")
+    except Exception as e:
+        print(f"{RED}An error occurred: {e}{END_COLOR}")
+    except KeyboardInterrupt:
+        print("Exiting...")
+        exit()
+
+while True:
+    main()
